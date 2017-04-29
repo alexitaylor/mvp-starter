f33eb4525f8e9ceecc20c0c979f85342de293253
alexitaylor <alexi@bluesunrise.com>
2017-04-28 21:30:39 -0700

    Create users schemas for Mongo w/ mongoose

--- src/database/User.model.js
+++ src/database/User.model.js
@@ -1,27 +1,34 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
 
-// create a schema
-// todo
+  // create a schema
+let user = new Schema({
+  username: { type: String, required: true, unique: true },
+  tempLow: Number,
+  tempHigh: Number,
+  windHigh: Number,
+  precipHigh: Number,
+  created_at: Date,
+  updated_at: Date
+});
 
 // on every save, add the date
-// schemaname.pre('save', function(next) {
-//   // get the current date
-//   var currentDate = new Date();
+schemaname.pre('save', function(next) {
+  // get the current date
+  let currentDate = new Date();
 
-//   // change the updated_at field to current date
-//   this.updated_at = currentDate;
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
 
-//   // if created_at doesn't exist, add to that field
-//   if (!this.created_at)
-//     this.created_at = currentDate;
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
 
-//   next();
-// });
+  next();
+});
 
 // the schema is useless so far
 // we need to create a model using it
-// todo
-
+let User = mongoose.model('User', userSchema);
 // make this available to our users in our Node applications
-// todo
+module.exports = User;
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,21 @@
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 // connect to a MongoDB database
 mongoose.connect('mongodb://localhost/users');
 
 // grab the user model
-var User = require('./User.model');
+let User = require('./User.model');
 
-// create new user
+// create new user hard coded for now
+let user = new User({
+  username: 'Nick',
+  tempLow: 50,
+  tempHigh: 100,
+  windHigh: 20,
+  precipHigh: .5
+})
 
 // Save new user
+user.save(err => {
+  if (err) throw err
+  console.log('User saved successfully')
+})
